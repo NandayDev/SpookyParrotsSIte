ce41c1f307e40b540afa15c9d6df2093d8ba271c
GitHub Push Action <push@no-reply.github.com>
2022-09-30 07:29:38 +0000

    GitHub Push

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,68 +3,69 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "51ca5cd535e42e71bbd8036dc21fbc4b",
+  "main.dart.js": "b55bdef79a23dbd892d7449092780d81",
 "CNAME": "29aef196e8236e18fc155f43d056da57",
-"style.css": "ffba23ff301032bec9a3922e763ddcca",
+"index.html": "6d09ebefebecc87e1911208e11b58adf",
+"/": "6d09ebefebecc87e1911208e11b58adf",
+"flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "manifest.json": "6cbe41637e331ef626d9a50c26f95fc1",
-"version.json": "db8990b8a733cdd52065ba1549ba9aa5",
-"flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
-"index.html": "523ad040e0b3c00683a45dad8455815e",
-"/": "523ad040e0b3c00683a45dad8455815e",
-"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
-"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
-"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
-"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
+"style.css": "a2d7ebe8db5f301b2850191c86085d49",
+"canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
+"canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
+"canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
+"canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
 "assets/AssetManifest.json": "9cb9256ca47f34ca98bfab10eb410459",
-"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
+"assets/shaders/ink_sparkle.frag": "0cc60a8f8615e45b11b1f78edf49c577",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/assets/home/constant/kofi.png": "c2b84425597a1c52b9d91627bcac697c",
-"assets/assets/home/constant/email.png": "0ae11c7c131c4152f3279d35526388ff",
-"assets/assets/home/constant/patreon.png": "d16c0e60162af902c49e8d833c97895c",
-"assets/assets/home/constant/instagram.png": "11fd5e27dbef0fc872e872b2d8828faf",
-"assets/assets/home/constant/twitter.png": "3a20f0fc87bf5ad851182d6681903770",
-"assets/assets/home/constant/twitch.png": "07f3a81d259bc36399667ab1e459e778",
-"assets/assets/projects/kofi.png": "18eab84f712861e838db43a0186d336a",
-"assets/assets/projects/gitaram.png": "e0c528ff1e67d954a6039ce204c38c8f",
-"assets/assets/projects/onelastnight.png": "ea413861a3d6bfa50df51e412ab97e70",
-"assets/assets/projects/stxaviers.png": "54936ceae932c0337c5c4096d2bf6037",
-"assets/assets/projects/rcciit.png": "e549f157d5783656caeb1f605259b897",
-"assets/assets/projects/downlight.png": "62295472e7ed5e365ddd7ea936c3b7f5",
-"assets/assets/portfolio.json": "34cdedcafb14c1cfcf5f7f7a6e6f659a",
-"assets/assets/fonts/SourceCodePro-SemiBold.ttf": "420d3580f5b6e63ba1eabb8555b5f6cf",
-"assets/assets/fonts/FjallaOne-Regular.ttf": "4fa3683005fa20dd058f47b9cfd411df",
-"assets/assets/fonts/Montserrat-Medium.ttf": "c8b6e083af3f94009801989c3739425e",
+"assets/FontManifest.json": "f0f4be009d67b49f6afb66286f706923",
+"assets/assets/experience/patreon.png": "5350cf08cd55456df04db0c76e588ed4",
+"assets/assets/experience/constant/experience.png": "8f6a768aa1f3a803088eff838313f7b0",
 "assets/assets/experience/opensource.png": "997db9ffb7f57a2c0ef1323c6056c44b",
-"assets/assets/experience/kofi.png": "5132bddcb9ce0828888c91a08f35a2ec",
+"assets/assets/experience/rcctechz.png": "40de2922a15538d96a1210fd9eb8457c",
 "assets/assets/experience/GDSC.png": "a144476dba333629f9a232e8c7cd64bf",
+"assets/assets/experience/kofi.png": "5132bddcb9ce0828888c91a08f35a2ec",
 "assets/assets/experience/coding-ninjas.png": "5b94a7d8ef90a9b60a3967745e044778",
 "assets/assets/experience/credanic.png": "27b5605cf2f3291e4cfe71f4f97daaf2",
-"assets/assets/experience/patreon.png": "5350cf08cd55456df04db0c76e588ed4",
-"assets/assets/experience/rcctechz.png": "40de2922a15538d96a1210fd9eb8457c",
-"assets/assets/experience/constant/experience.png": "8f6a768aa1f3a803088eff838313f7b0",
-"assets/assets/contact_me/picture.png": "13a4b677d9fa726efd9e771189222bf5",
 "assets/assets/contact_me/spookyparrots.png": "50b67e296839a45a466013dab2162b9c",
+"assets/assets/contact_me/constant/location-dark.png": "08a65e7c79b85fbcb40e8fce00953e47",
 "assets/assets/contact_me/constant/picture.png": "1a817a95a42d8c43031378d122a05ffe",
 "assets/assets/contact_me/constant/location.png": "dbc10b089546daaee5c45d9bb754f989",
 "assets/assets/contact_me/constant/ciocce.jpg": "b186b11c8fbbd8e48f79dbb79a54bebb",
-"assets/assets/contact_me/constant/location-dark.png": "08a65e7c79b85fbcb40e8fce00953e47",
-"assets/assets/crew/pappi.png": "c578dab1e06810035724bd79a84e0e9c",
-"assets/assets/crew/guido.png.bak": "b3b3825b2c94a506e8c9aee976000e43",
+"assets/assets/contact_me/picture.png": "13a4b677d9fa726efd9e771189222bf5",
+"assets/assets/crew/guido.png": "30ee04beb698fdcb56ba46b330fe253d",
 "assets/assets/crew/valerio.png": "cafa38532e92d2e98354032744ec27f8",
 "assets/assets/crew/adelina.png": "0df33e3e1ebe47192efcdec14d72d7d1",
-"assets/assets/crew/guido.png": "30ee04beb698fdcb56ba46b330fe253d",
-"assets/assets/what_i_do/java.png": "53b82a97cf4e13fac807691e7b0be082",
+"assets/assets/crew/guido.png.bak": "b3b3825b2c94a506e8c9aee976000e43",
+"assets/assets/crew/pappi.png": "c578dab1e06810035724bd79a84e0e9c",
 "assets/assets/what_i_do/git.png": "2f2db226d2f03acc82b77c7fe652fd3b",
-"assets/assets/what_i_do/linux.png": "0dda41d21f571add8b3a558c9914c8ff",
-"assets/assets/what_i_do/python.png": "d464079775fdeabf0ffb1323b2a02d2c",
 "assets/assets/what_i_do/cpp.png": "efeec8be519a45426ed24cd91b86fc53",
 "assets/assets/what_i_do/bash.png": "8aec2de4c0634c93202ec36d7ac4a5de",
-"assets/assets/what_i_do/flutter.png": "5b26614dbdbcf4b3ecb80bed00ad3702",
-"assets/assets/what_i_do/c.png": "9f6665a404d1b2384b4a90419ad08fd2",
 "assets/assets/what_i_do/constant/checklist.png": "bce0e7781492fe752297f8369c666494",
 "assets/assets/what_i_do/constant/checklist-light.png": "c6a80a3ff27e7db249fb88159383f74d",
-"assets/NOTICES": "ef1df6efd40bfa4fbb5b1d8809fd8c80",
-"assets/FontManifest.json": "f0f4be009d67b49f6afb66286f706923"
+"assets/assets/what_i_do/flutter.png": "5b26614dbdbcf4b3ecb80bed00ad3702",
+"assets/assets/what_i_do/c.png": "9f6665a404d1b2384b4a90419ad08fd2",
+"assets/assets/what_i_do/linux.png": "0dda41d21f571add8b3a558c9914c8ff",
+"assets/assets/what_i_do/java.png": "53b82a97cf4e13fac807691e7b0be082",
+"assets/assets/what_i_do/python.png": "d464079775fdeabf0ffb1323b2a02d2c",
+"assets/assets/projects/gitaram.png": "e0c528ff1e67d954a6039ce204c38c8f",
+"assets/assets/projects/kofi.png": "18eab84f712861e838db43a0186d336a",
+"assets/assets/projects/onelastnight.png": "ea413861a3d6bfa50df51e412ab97e70",
+"assets/assets/projects/downlight.png": "62295472e7ed5e365ddd7ea936c3b7f5",
+"assets/assets/projects/stxaviers.png": "54936ceae932c0337c5c4096d2bf6037",
+"assets/assets/projects/rcciit.png": "e549f157d5783656caeb1f605259b897",
+"assets/assets/portfolio.json": "34cdedcafb14c1cfcf5f7f7a6e6f659a",
+"assets/assets/fonts/Montserrat-Medium.ttf": "c8b6e083af3f94009801989c3739425e",
+"assets/assets/fonts/FjallaOne-Regular.ttf": "4fa3683005fa20dd058f47b9cfd411df",
+"assets/assets/fonts/SourceCodePro-SemiBold.ttf": "420d3580f5b6e63ba1eabb8555b5f6cf",
+"assets/assets/home/constant/patreon.png": "d16c0e60162af902c49e8d833c97895c",
+"assets/assets/home/constant/kofi.png": "c2b84425597a1c52b9d91627bcac697c",
+"assets/assets/home/constant/instagram.png": "11fd5e27dbef0fc872e872b2d8828faf",
+"assets/assets/home/constant/twitch.png": "07f3a81d259bc36399667ab1e459e778",
+"assets/assets/home/constant/twitter.png": "3a20f0fc87bf5ad851182d6681903770",
+"assets/assets/home/constant/email.png": "0ae11c7c131c4152f3279d35526388ff",
+"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
+"assets/NOTICES": "75ad35b26435dd8f4c389a78d52f5d42",
+"version.json": "db8990b8a733cdd52065ba1549ba9aa5"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -72,7 +73,6 @@ const RESOURCES = {
 const CORE = [
   "main.dart.js",
 "index.html",
-"assets/NOTICES",
 "assets/AssetManifest.json",
 "assets/FontManifest.json"];
 // During install, the TEMP cache is populated with the application shell files.
@@ -171,9 +171,11 @@ self.addEventListener("fetch", (event) => {
     .then((cache) =>  {
       return cache.match(event.request).then((response) => {
         // Either respond with the cached resource, or perform a fetch and
-        // lazily populate the cache.
+        // lazily populate the cache only if the resource was successfully fetched.
         return response || fetch(event.request).then((response) => {
-          cache.put(event.request, response.clone());
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
           return response;
         });
       })
